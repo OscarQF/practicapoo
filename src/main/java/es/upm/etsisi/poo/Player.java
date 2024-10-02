64323f880fcbed18458f6778cfa25534e34d0214
Kevinrob88 <86365571+Kevinrob88@users.noreply.github.com>
2024-10-02 12:33:18 +0200

    Update and rename Jugador.java to Player.java

---
+++ src/main/java/es/upm/etsisi/poo/Player.java
@@ -0,0 +1,51 @@
+package es.upm.etsisi.poo;
+
+public class Player {
+    private String name;
+    private double score;
+    private boolean matchmaked;
+
+    public Player(){
+        this.name = "";
+        this.score = 0.0;
+        this.matchmaked = false;
+    }
+
+    public Player(String name, double score, boolean matchmaking) {
+        this.name = name;
+        this.score = score;
+        this.matchmaked = matchmaking;
+    }
+
+    // METODOS
+    public Player[] rankingPlayers(Player[] arrayPlayers){
+        return null;
+    }
+    // GETTERS Y SETTERS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public boolean getMatchmaked() {
+        return matchmaked;
+    }
+
+    public void setMatchmaking(boolean matchmaking) {
+        this.matchmaked = matchmaking;
+    }
+
+
+
+}
